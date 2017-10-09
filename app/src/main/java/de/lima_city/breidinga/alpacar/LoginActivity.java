79d216705217c16a8e8373d48cb54d69134192b1
addie9800 <ad123br@gmail.com>
2017-10-09 16:53:03 +0200

    Registration
    Sorry, dass das so lange gedauert hat, aber hier jetzt die Registration
    auch dabei.

--- app/src/main/java/de/lima_city/breidinga/alpacar/LoginActivity.java
+++ app/src/main/java/de/lima_city/breidinga/alpacar/LoginActivity.java
@@ -8,8 +8,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -35,7 +38,14 @@ import static android.R.string.ok;
 public class LoginActivity extends AppCompatActivity {
     EditText user;
     EditText pass;
+    EditText vorname;
+    EditText nachname;
+    EditText mail;
+    LinearLayout loginButtonLayout;
+    Button loginButton;
     String ans;
+    String username;
+    String password;
     @Override
     public void onBackPressed() {
         finish();
@@ -47,6 +57,117 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         user = (EditText) findViewById(R.id.user);
         pass = (EditText) findViewById(R.id.pass);
+        vorname = (EditText) findViewById(R.id.Vorname);
+        nachname = (EditText) findViewById(R.id.Nachname);
+        mail = (EditText) findViewById(R.id.mail);
+        loginButtonLayout = (LinearLayout) findViewById(R.id.buttonLayout);
+        loginButton = (Button) findViewById(R.id.loginButton);
+    }
+    public void register(View view){
+        if (user.getText().toString().isEmpty() || pass.getText().toString().isEmpty()){
+            return;
+        }
+        username = user.getText().toString();
+        password = pass.getText().toString();
+        user.setVisibility(View.GONE);
+        pass.setVisibility(View.GONE);
+        loginButtonLayout.setVisibility(View.GONE);
+        vorname.setVisibility(View.VISIBLE);
+        nachname.setVisibility(View.VISIBLE);
+        mail.setVisibility(View.VISIBLE);
+        loginButton.setVisibility(View.VISIBLE);
+
+    }
+    public void sendDataButton(View view){
+        if (vorname.getText().toString().isEmpty() || nachname.getText().toString().isEmpty() || mail.getText().toString().isEmpty()){
+            Toast.makeText(this, "Eine oder mehrere Felder nicht ausgefüllt", Toast.LENGTH_LONG).show();
+            return;
+        }
+        CharSequence mailtext = mail.getText();
+        if(!Patterns.EMAIL_ADDRESS.matcher(mailtext).matches()){
+            Toast.makeText(this, "Bitte eine gültige E-mail Adresse angeben",  Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String vname = vorname.getText().toString();
+        String nname = nachname.getText().toString();
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("https")
+                .authority("breidinga.lima-city.de")
+                .appendPath("Datenbank")
+                .appendPath("nutzer.php");
+        Uri baseUri = builder.build();
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("mode", "register");
+        uriBuilder.appendQueryParameter("user", username);
+        uriBuilder.appendQueryParameter("pass", password);
+        uriBuilder.appendQueryParameter("vname", vname);
+        uriBuilder.appendQueryParameter("nname", nname);
+        uriBuilder.appendQueryParameter("email", mailtext.toString());
+        final Uri uri = uriBuilder.build();
+        AsyncTask<Uri, Void,Void> asyncTask = new AsyncTask<Uri, Void, Void>() {
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("login", true);
+                    Toast.makeText(getBaseContext(), "Registration successful", Toast.LENGTH_LONG).show();
+                    finish();
+                    startActivity(intent);
+                }
+            @Override
+            protected Void doInBackground(Uri... uris) {
+                URL url;
+                url = null;
+                try{
+                    url = new URL(uri.toString());}
+                catch (MalformedURLException e){
+                    e.printStackTrace();
+                }
+                if(url == null){
+                    Log.e("MainActivity.java", "Error creating URL");
+                    return null;
+                }
+
+
+                // new try
+                HttpURLConnection urlConnection = null;
+                InputStream inputStream = null;
+                try {
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setReadTimeout(10000 /* milliseconds */);
+                    urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.connect();
+                    if (urlConnection.getResponseCode() == 200) {
+                        inputStream = urlConnection.getInputStream();
+                        ans = readFromStream(inputStream);
+                        Log.d("test", ans);
+                    } else {
+                        Log.e("QueryUtils", "Error response code: " + urlConnection.getResponseCode());
+                    }
+
+
+                    // TODO: Parse the response given by the SAMPLE_JSON_RESPONSE string and
+                    // build up a list of Earthquake objects with the corresponding data.
+
+                }
+                catch (ProtocolException e){
+                    Log.e("QueryUtils", "Problem with the protocol", e);
+                }
+                catch (IOException e){
+                    Log.e("QueryUtils", "Problem establishing the connection", e);
+
+                }
+                finally {
+                    if (urlConnection != null){
+                        urlConnection.disconnect();
+                    }
+                }
+                return null;
+            }
+        };
+        asyncTask.execute(uri);
     }
     public void login (View view){
         if (user.getText().toString().isEmpty() || pass.getText().toString().isEmpty()){
